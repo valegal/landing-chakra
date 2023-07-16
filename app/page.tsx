d02355f572932e4636197eb97557d54f7b937d8b
Valentina Galeano <valentinagaleanow@gmail.com>
2023-07-16 15:09:39 -0500

    fix1

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 "use client"
+
 import Features from "@/components/Features";
 import Hero from "@/components/HeroSection";
 import Testimonials from "@/components/Testimonials";
-
-
 
 export default function Home() {
   return (
